2b7f2b5b043cd7b9ef32f0adf42c4470747dff3c
Priyanshi <b22064@students.iitmandi.ac.in>
2024-02-11 15:07:36 +0530

    backend

---
+++ src/strategy.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const User = require("../schema/user");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const callbackuri = process.env.NODE_ENV === "production" ? process.env.CALLBACK_URL:"http://localhost:8080/auth/google/CS";
+const strategy = new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: callbackuri,
+      userProfileUrl: "https://www.googleapis.com.oauth2.v3.userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate(
+        { username: profile.id },
+        {
+          name: profile._json.name,
+          pic: profile._json.picture,
+          email: profile._json.email,
+        },
+        function (err, user) {
+          console.log(profile.displayName);
+          return cb(err, user);
+        }
+      );
+    }
+  );
+
+  module.exports = strategy;
